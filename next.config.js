b848598a7c8b502d3f86f76d409c3c501da9a71c
Blessing Uzoukwu <uzoukwublessing34@gmail.com>
2026-05-11 18:35:43 +0000

    feat: Configure GitHub Pages deployment with static export

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/blessing-portfolio',
   images: {
     remotePatterns: [
       {
@@ -7,6 +9,7 @@ const nextConfig = {
         hostname: '**',
       },
     ],
+    unoptimized: true,
   },
 };
 
